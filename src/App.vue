899eb241359b784c809d9647b725d88f2b49a539
woodoo2013 <woodoo2013@gmail.com>
2021-01-17 21:14:58 +0700

    slider without buttons

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <slider></slider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slider from "@/components/Slider";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slider,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+
+
+
+    <div class="slider-item-wrapper">
+      <div v-for="(slide, index) in slides" :key="slide.background">
+        <transition name="slide-fade">
+          <div v-if="currentSlide == index"
+               :style="{backgroundColor: slide.background}"
+               class="slider-item">
+          </div>
+        </transition>
+      </div>
+    </div>
+
+
+
+
+    <div class="slide-dots-wrapper">
+      <div v-for="(slideDot, index) in slideDots" :key="slideDot">
+        <div @click="changeSlide(index)" class="slide-dot" :class="index === currentSlide ? 'activeSlideDot' : 'inActiveSlideDot'">
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  data() {
+    return {
+      slides: [
+        {background: 'blue'},
+        {background: 'yellow'},
+        {background: 'green'}
+      ],
+      currentSlide: 0,
+      sliderTimeOut: '',
+      slideDots: 3
+    }
+  },
+  methods: {
+    changeSlide(slide) {
+      this.currentSlide = slide
+    }
+  },
+  mounted() {
+    this.sliderTimeOut = setInterval(() => {
+      this.currentSlide = this.currentSlide === 2 ? 0 : this.currentSlide + 1
+    }, 3000)
+  },
+  beforeUnmount() {
+    clearTimeout(this.sliderTimeOut)
+  }
+}
+</script>
+
+<style scoped>
+
+.slider-item {
+  width: 100%;
+  height: 500px;
+  position: absolute;
+  top: 0;
+}
+
+.slider-item-wrapper {
+  height: 440px;
+}
+
+.slide-dots-wrapper {
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+}
+
+.activeSlideDot {
+  background: white;
+}
+
+.inActiveSlideDot {
+  background: black;
+}
+
+.slide-dot {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 5px;
+  opacity: 0.9;
+  cursor: pointer;
+}
+
+.slide-fade-enter-active {
+  transition: all 1s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all 1s ease;
+}
+
+.slide-fade-enter-from {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.slide-fade-leave-to {
+  transform: translateX(100%);
+  opacity: 0;
+}
+</style>
